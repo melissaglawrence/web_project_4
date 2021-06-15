bbbec6c3bebc09e10f8332485f5a777b748f9bd7
Melissa Lawrence <melissaglawrence8@gmail.com>
2021-06-15 12:56:52 -0700

    refactor: form

--- script/script.js
+++ script/script.js
@@ -1,35 +1,32 @@
-let main = document.querySelector(".main");
-let edit = main.querySelector(".profile__edit");
-let profileName = main.querySelector(".profile__name");
-let profileDesc = main.querySelector(".profile__desc");
+let page = document.querySelector(".page");
+let edit = page.querySelector(".profile__edit");
+let profileName = page.querySelector(".profile__name");
+let profileDesc = page.querySelector(".profile__desc");
 let form = document.querySelector(".form");
 let formName = form.querySelector(".form__name");
 let formDesc = form.querySelector(".form__desc");
-let close = form.querySelector(".close");
+let closeBttn = form.querySelector(".close");
 let save = form.querySelector(".form__save");
 
 function popup(evt){
     formName.value = profileName.textContent;
     formDesc.value = profileDesc.textContent;
     form.classList.remove("visable");
-    main.setAttribute("style", "opacity:50%;");
     evt.preventDefault();
 }
 
 function closeForm(evt){
-evt.preventDefault();
-form.classList.add("visable");
-main.setAttribute("style", "opactiy:100%;");
+    evt.preventDefault();
+    form.classList.add("visable");
 }
 
 function saveForm(evt){
-profileDesc.textContent = formDesc.value;
-profileName.textContent = formName.value;
-form.classList.add("visable");
-main.setAttribute("style", "opactiy:100%;");
-evt.preventDefault();
+    profileDesc.textContent = formDesc.value;
+    profileName.textContent = formName.value;
+    form.classList.add("visable");
+    evt.preventDefault();
 }
 
 save.addEventListener("click", saveForm);
-close.addEventListener("click", closeForm);
+closeBttn.addEventListener("click", closeForm);
 edit.addEventListener("click", popup);
