7d218a054f490a2c7196601196735c867cc8b5ec
Melissa Lawrence <melissaglawrence8@gmail.com>
2021-06-18 01:56:14 -0700

    refactor: file paths

--- script/script.js
+++ script/script.js
@@ -8,20 +8,20 @@ let popupDesc = popup.querySelector(".popup__input_desc_edit");
 let closeBttn = popup.querySelector(".popup__close");
 let save = popup.querySelector(".popup__save");
 
-function popupForm(evt){
+function popupForm(){
     popupName.value = profileName.textContent;
     popupDesc.value = profileDesc.textContent;
-    popup.classList.remove("popup__opened");
+    popup.classList.remove("popup_opened");
 }
 
-function closeForm(evt){
-    popup.classList.add("popup__opened");
+function closeForm(){
+    popup.classList.add("popup_opened");
 }
 
 function saveForm(evt){
     profileDesc.textContent = popupDesc.value;
     profileName.textContent = popupName.value;
-    popup.classList.add("popup__opened");
+    popup.classList.add("popup_opened");
     evt.preventDefault();
 }
 
